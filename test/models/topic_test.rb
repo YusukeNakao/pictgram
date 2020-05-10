b084706506122307c5b1aa7fa16ae64ff45b584b
Yusuke Nakao <ynakao8228@gmai.com>
2020-05-10 03:10:13 +0000

    Modified sourcecode

--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,17 +1,7 @@
-require 'test_helper' < ApplicationRecord
-  
+require 'test_helper'
+
 class TopicTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
-
-class Topic < ApplicationRecord
-  varidates :user_id, presence: true
-  varidates "description, presence: true"
-  varidates "image, presence: true"
-  
-  belongs_to :user
-  
-  mount_uploader :image, ImageUploader
 end
